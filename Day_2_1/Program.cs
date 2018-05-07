54649186e21bcd06c0a43cc9acb2b007be0deaf7
astikute <agnese.stikute@gmail.com>
2018-05-07 20:01:18 +0300

    Day 2
    Pirmais commits 2.dienai

---
+++ Day_2_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ievadiet savu dzimsanas dienas datumu!");
+            string diena = Console.ReadLine();
+            Console.WriteLine("Ievadiet savu dzimsanas menesi!");
+            string menesis = Console.ReadLine();
+            Console.WriteLine("Ievadiet savu dzimsanas gadu!");
+            string gads = Console.ReadLine();
+            Console.WriteLine("Jusu dzimsanas diena ir: " + diena + menesis + " " + gads);
+            Console.Read();
+        }
+    }
+}
